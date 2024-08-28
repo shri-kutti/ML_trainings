99034be9bfe629eda8a4186bc6010d184f774d90
Shri Jayaram <107614089+shri-kutti@users.noreply.github.com>
2024-08-28 11:04:18 +0530

    added gitignore-remove venv chunking

---
+++ chuk.py
@@ -0,0 +1,38 @@
+import re
+
+def word_splitter(paragraph : str) -> list[str]:
+    words = re.sub("\s+"," ",paragraph) # to check that ultiple spaces are removed coz u know s\+ ...
+    return re.split("\s",words)
+
+chukns = []
+
+# def chuker(seprated : list[str],chuk_size: int) -> list[str]:
+#     for i in range(0,len(seprated),chuk_size):
+#         part = seprated[i:i+chuk_size]
+#         chukns.append(part)
+    
+#     return chukns
+
+# atr = "TThus i sa saloi   as in palaidin ahead injulin oklama juvie"
+
+# emi = word_splitter(atr)
+# print(chuker(emi,2))
+
+import requests
+#https://weaviate.io/developers/academy/py/standalone/chunking/how_1
+def chuker(seperated : list[str], chuk_size : int ,overlap_fraction : float) -> list[str]:
+    overlap_int = int(chuk_size * overlap_fraction)
+    for i in range(0,len(seperated),chuk_size):
+        next = seperated[max(i-overlap_int,0):i+chuk_size]
+        # part = " ".join(next)
+        # print(part)
+        chukns.append(" ".join(next))
+    return chukns
+
+url = "https://raw.githubusercontent.com/progit/progit2/main/book/01-introduction/sections/what-is-git.asc"
+src_txt = requests.get(url).text
+
+spe = word_splitter(src_txt)
+print(chuker(spe,5,0.4))
+
+# repr() - > to  represent a string representation of an object it includes all the special characters toooo.
